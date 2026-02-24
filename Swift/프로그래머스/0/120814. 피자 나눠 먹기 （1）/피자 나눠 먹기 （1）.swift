f249b162b9d458fbd41bee078442013b1690708d
Demian YOO (유경모) <98875580+YooGyeongMo@users.noreply.github.com>
2026-02-24 14:23:11 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

--- Swift/프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.swift
+++ Swift/프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.swift
@@ -2,13 +2,5 @@ import Foundation
 
 func solution(_ n:Int) -> Int {
     
-    switch n {
-        case 1..<7:
-            return 1
-        case _ where n%7==0:
-            return n/7
-        default:
-            return n / 7 + 1
-    }
-    return 0
+    return Int(ceil(Double(n) / Double(7)))
 }
