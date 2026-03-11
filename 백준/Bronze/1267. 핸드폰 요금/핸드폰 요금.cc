fe992b5800b092a0bea759cb842e79e781e3cb01
Demian YOO (유경모) <98875580+YooGyeongMo@users.noreply.github.com>
2026-03-11 23:38:20 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int y = 0;
+    int m = 0;
+
+    for (int i = 0; i < n; i++) {
+        int t;
+        cin >> t;
+
+        y += (t / 30 + 1) * 10;
+        m += (t / 60 + 1) * 15;
+    }
+
+    if (y < m) {
+        cout << "Y " << y;
+    } else if (m < y) {
+        cout << "M " << m;
+    } else {
+        cout << "Y M " << y;
+    }
+
+    return 0;
+}
