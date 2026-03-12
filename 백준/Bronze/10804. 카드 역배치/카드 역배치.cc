10001c051950b85db3516a4a547de8bac4b245a7
Demian YOO (유경모) <98875580+YooGyeongMo@users.noreply.github.com>
2026-03-12 11:42:02 +0900

    [Bronze II] Title: 카드 역배치, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10804. 카드 역배치/카드 역배치.cc
+++ 백준/Bronze/10804. 카드 역배치/카드 역배치.cc
@@ -3,11 +3,18 @@
 
 using namespace std;
 
+int arr[21];
+
+void reverse (int a, int b) {
+    for (int i = 0; i < (b-a+1)/2; i++) {
+        int temp = arr[a+i];
+        arr[a+i] = arr[b-i];
+        arr[b-i] = temp;
+    }
+}
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-
-    int arr[21];
 
     for (int i = 1; i<21; i++ ) {
         arr[i] = i;
@@ -16,8 +23,10 @@ int main(void) {
     for (int i = 0; i<10; i++) {
         int a, b;
         cin >> a >> b;
-        reverse(arr+a,arr+b+1);
+        // reverse(arr+a,arr+b+1);
+        reverse(a,b);
     }
+
 
     for (int i = 1; i<21; i++) {
         cout << arr[i] << " ";
