aaded729a6494b2ff3e3d76094d6db083912131e
Demian YOO (유경모) <98875580+YooGyeongMo@users.noreply.github.com>
2026-03-12 12:07:59 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+
+    cin >> N;
+
+    for (int i = N; i>0; i--) {
+        for (int j = 0; j<i; j++ ) {
+            cout << "*";
+        }
+        cout <<"\n";
+    }
+
+}
