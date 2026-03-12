43a9a1ed9936ad038c03a3d7010b16a42b478e32
Demian YOO (유경모) <98875580+YooGyeongMo@users.noreply.github.com>
2026-03-12 12:45:44 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -11,11 +11,11 @@ int main(void) {
 
     cin >> N;
 
-    for (int i = 1; i<=N; i++) {
-        for (int j = 1; j<i; j++) {
+    for (int i = N; i > 0; i--) {
+        for (int j = 0; j < N-i; j++) {
             cout << " ";
         }
-        for (int j = 1; j <= 2*(N-i)+1; j++) {
+        for (int j = 0; j < 2*i-1; j++) {
             cout << "*";
         }
         cout << "\n";
